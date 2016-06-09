c54af75e7fe517c2ecc508a4671094ad3a2ff9cf
Oliver Marks <oly@digitaloctave.com>
2016-06-09 21:25:13 +0100

    new test images

--- python/image_send_simple.py
+++ python/image_send_simple.py
@@ -9,7 +9,7 @@ import time
 #dsrdtr setting this true will reset the arduino
 
 ser = serial.Serial(
-    port='/dev/ttyACM0',
+    port='/dev/ttyACM1',
     baudrate=115200,
     timeout=0,
     rtscts=True, dsrdtr=True
@@ -24,10 +24,10 @@ def drawImage(im):
     for row in im:
         result.append(''.join([str(num) for num in row]))
     return result
-    #~ for column in row:
-        #~ print p
-            #~ offset = row
-            #~ ser.write(chr(display) + chr(row + 1) + chr(offset))
+
+def clear(display=0):
+    ser.write(chr(display) + chr(0) + chr(0))
+    s = ser.read(ser.inWaiting())
 
 def drawList(data, display=0):
     for row in range(0, 8):
@@ -35,19 +35,32 @@ def drawList(data, display=0):
         ser.write(chr(display) + chr(row + 1) + chr(offset))
         s = ser.read(ser.inWaiting())
 
-im = Image.open("grid.png")
-im_array = np.array(im)
-im_array[im_array > 128] = 1 # any value in the array greater than 128 change it to the value 1
-im_formatted = drawImage(im_array)
+def loadImage(filename):
+    im = Image.open(filename)
+    im_array = np.array(im)
+    im_array[im_array < 128] = 1 # any value in the array greater than 128 change it to the value 1
+    im_array[im_array > 128] = 0 # any value in the array greater than 128 change it to the value 1
+    return drawImage(im_array)
 
+invader1 = loadImage('invader1.png')
+invader2 = loadImage('invader2.png')
+
+print invader1
+print invader2
 
 while True:
-    for disp in range(0, 2):
-        drawList(drawImage(im_array), 0)
-        time.sleep(1)
+    #~ clear(0)
+    #~ clear(1)
+    drawList(invader1, 0)
+    drawList(invader2, 1)
+    time.sleep(1)
 
-        # flip the array
-        drawList(drawImage(im_array.T), 0)
-        time.sleep(1)
+    #~ for disp in range(0, 2):
+    #~ clear(0)
+    #~ clear(1)
+    #~ drawList(invader2, 0)
+    #~ drawList(invader1, 1)
+    #~ time.sleep(1)
+    #~ 
 
 ser.close()
